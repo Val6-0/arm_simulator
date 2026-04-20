9dc9124168082868eabc9707b656f2250e7bf2f3
Valentin Sauzede <valentin.sauzede07@gmail.com>
2026-04-20 08:17:07 +0200

    added SUB support

--- execution.c
+++ execution.c
@@ -50,6 +50,28 @@ void execution(linked_L *list, environment *ENV) {
             ENV->reg_array[reg_mem] = sum;
             list = list->next;
             break;
+        case SUB:
+            list = list->next;
+            reg_mem = list->cell_data.val;
+            list = list->next; // comma skip
+            list = list->next;
+            sum = ENV->reg_array[list->cell_data.val]; // rt
+            list = list->next;                         // comma skip
+            list = list->next;
+            switch (list->cell_data.word_t) { // rt - rn
+            case REG:
+                sum -= ENV->reg_array[list->cell_data.val];
+                break;
+            case VALUE:
+                sum -= list->cell_data.val;
+                break;
+            default:
+                printf("error during execution (shouldn't be possible)\n");
+                exit(0);
+            }
+            ENV->reg_array[reg_mem] = sum;
+            list = list->next;
+            break;
         default:
             printf("error during execution (shouldn't be possible)\n");
             exit(0);
---
+++ test_files/test_SUB.s
@@ -0,0 +1,4 @@
+MOV r0, #10
+MOV r1, #2
+SUB r2, r0, r1
+SUB r3, r2, #3
